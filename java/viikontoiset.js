c20a0c162f14e1f1af28fdd5749a872e34467309
electricitas <142588614+electricitas@users.noreply.github.com>
2023-09-22 11:30:32 +0300

    Add files via upload

--- java/viikontoiset.js
+++ java/viikontoiset.js
@@ -55,3 +55,29 @@ function tarkistaKarkausvuosi() {
         }
     }
 }
+
+function laskeSummaJaKeskiarvo() {
+    var luku1 = parseInt(document.getElementById("luku1").value);
+    var luku2 = parseInt(document.getElementById("luku2").value);
+    var luku3 = parseInt(document.getElementById("luku3").value);
+    var luku4 = parseInt(document.getElementById("luku4").value);
+    var luku5 = parseInt(document.getElementById("luku5").value);
+
+    var summa = luku1 + luku2 + luku3 + luku4 + luku5;
+    var keskiarvo = summa / 5;
+
+    var tulosElementti = document.getElementById("tulo");
+    tulosElementti.innerHTML = "Lukujen summa on: " + summa + " ja keskiarvo on: " + keskiarvo;
+}
+
+function laskeKertotaulu() {
+    var luku = parseInt(document.getElementById("luku").value);
+    var lause = "";
+
+    for (var i = 1; i <= 10; i++) {
+        lause += luku + "x " + i + " = " + (luku * i) + "<br>";
+    }
+
+    var lauseElementti = document.getElementById("lause");
+    lauseElementti.innerHTML = lause;
+}
