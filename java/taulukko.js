a33facbe2af35527994ae403937442c4219a318a
electricitas <142588614+electricitas@users.noreply.github.com>
2023-11-01 14:58:42 +0200

    Add files via upload

---
+++ java/taulukko.js
@@ -0,0 +1,22 @@
+﻿const lahetaNappi = document.getElementById("lahetaNappi");
+
+lahetaNappi.addEventListener("click", function () {
+    const etunimi = document.getElementById("etu").value;
+    const sukunimi = document.getElementById("suku").value;
+
+    console.log("Etunimi: " + etunimi);
+    console.log("Sukunimi: " + sukunimi);
+});
+
+const form = document.querySelector(".kouluun form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const selectedOption = document.querySelector("input[name='kulkuvalinta']:checked");
+    if (selectedOption) {
+        const selectedValue = selectedOption.value;
+        console.log("Valitsit kulkuvalinnaksi: " + selectedValue);
+    } else {
+        console.log("Valitse kulkuvalinta ennen kuin jatkat.");
+    }
+});
